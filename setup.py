8caad626519e961ff4c4bfa3e0de7f3444d1a128
Sheng Zhong <szhong@kepler.space>
2018-08-08 16:52:33 -0400

    Add more information to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
 from setuptools import setup, find_packages
 
-setup(name='binary_search',
-      version='0.1.0',
-      # typing
-      python_requires='>=3.5.*',
-      packages=find_packages(),
-      test_suite='pytest',
-      tests_require=[
-          'pytest',
-      ],
-      )
+setup(
+    name='binary_search',
+    description='Binary search on python sorted sequences',
+    version='0.1.0',
+    url='https://github.com/LemonPi/python_binary_search',
+    author='Sheng Zhong',
+    author_email='zhsh@umich.edu',
+    # typing
+    python_requires='>=3.5.*',
+    packages=find_packages(),
+    test_suite='pytest',
+    tests_require=[
+        'pytest',
+    ],
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+    ],
+)
